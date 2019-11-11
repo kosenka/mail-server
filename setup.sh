195df0cac1e88f8e52eedc6161f82528c622e2e8
kosenka <kosenka@users.noreply.github.com>
2019-11-11 15:46:37 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -35,7 +35,7 @@ sed -i "s/'configured'] = false/'configured'] = true/g" /var/www/html/postfixadm
 sed -i "s/= 'md5crypt'/= 'CRAM-MD5'/g" /var/www/html/postfixadmin/config.inc.php
 sed -i "s/database_user'] = 'postfix'/database_user'] = '$POSTFIX_DB_USER'/g" /var/www/html/postfixadmin/config.inc.php
 sed -i "s/database_password'] = 'postfixadmin'/database_password'] = '$POSTFIX_DB_PASS'/g" /var/www/html/postfixadmin/config.inc.php
-sed -i "s/database_name'] = 'postfix'/database_password'] = '$POSTFIX_DB_NAME'/g" /var/www/html/postfixadmin/config.inc.php
+sed -i "s/database_name'] = 'postfix'/database_name'] = '$POSTFIX_DB_NAME'/g" /var/www/html/postfixadmin/config.inc.php
 
 #create database for PostFix
 mysql --user="$DB_USER" --password="$DB_PASS" --execute="CREATE USER '$POSTFIX_DB_USER'@'%' IDENTIFIED BY '***';GRANT ALL PRIVILEGES ON *.* TO '$POSTFIX_DB_USER'@'%' IDENTIFIED BY '***' REQUIRE NONE WITH GRANT OPTION MAX_QUERIES_PER_HOUR 0 MAX_CONNECTIONS_PER_HOUR 0 MAX_UPDATES_PER_HOUR 0 MAX_USER_CONNECTIONS 0;CREATE DATABASE IF NOT EXISTS `$POSTFIX_DB_NAME`;GRANT ALL PRIVILEGES ON `$POSTFIX_DB_NAME`.* TO '$POSTFIX_DB_USER'@'%';"
