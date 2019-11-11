1d03edd54950ab9a722e7d591b305f48846cf0df
kosenka <kosenka@users.noreply.github.com>
2019-11-11 13:22:31 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,12 @@
+#!/bin/bash
+
+#
+# https://serveradmin.ru/nastroyka-postfix-dovecot-centos-7/
+#
+
+DOMAIN = "argus-fito.ru"
+MAIL_DOMAIN = "mail."$DOMAIN
+
 yum install httpd php phpmyadmin mariadb mariadb-server php-imap
 systemctl start httpd
 systemctl enable httpd
@@ -6,3 +15,8 @@ systemctl enable mariadb
 /usr/bin/mysql_secure_installation
 cd /usr/src
 wget https://downloads.sourceforge.net/project/postfixadmin/postfixadmin/postfixadmin-3.0.2/postfixadmin-3.0.2.tar.gz
+tar -xvzf postfixadmin-*
+mv /usr/src/postfixadmin-3.0.2 /var/www/html/postfixadmin
+chown -R apache. /var/www/html/postfixadmin/
+sed -i 's/$CONF['encrypt'] = 'md5crypt';/$CONF['encrypt'] = 'CRAM-MD5';/g' /var/www/html/postfixadmin/config.inc.php
+
